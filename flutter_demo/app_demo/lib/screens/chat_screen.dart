5c9b96b6346f94bde285c219346b6db8184c3f36
Marc Franquesa <marc.franquesam@gmail.com>
2022-11-05 15:38:17 +0100

    Init chat

--- flutter_demo/app_demo/lib/screens/chat_screen.dart
+++ flutter_demo/app_demo/lib/screens/chat_screen.dart
@@ -1,13 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class ChatScreen extends StatelessWidget {
+import '../models/models.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  late User user;
+  late Chat chat;
+
+  @override
+  void initState() {
+    user = Get.arguments[0];
+    chat = Get.arguments[1];
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.topRight,
+          colors: [
+            Theme.of(context).colorScheme.primary,
+            Theme.of(context).colorScheme.secondary,
+          ],
+        ),
+      ),
+      child: Scaffold(
+          appBar: _CustomAppBar(
+            user: user,
+          ),
+          body: Column(children: [])),
     );
   }
+}
+
+class _CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const _CustomAppBar({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        title: Column(
+          children: [
+            Text(
+              '${user.name} ${user.surname}',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              'Online',
+              style: Theme.of(context).textTheme.bodySmall,
+            )
+          ],
+        ),
+        elevation: 0,
+        actions: [
+          Container(
+              margin: const EdgeInsets.only(right: 10),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(user.imageUrl),
+              ))
+        ]);
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56.0);
 }
