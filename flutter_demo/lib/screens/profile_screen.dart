e26fdd3e5ed5eea1b7f9cbae73c2b79ff9dd8386
maurofr <m.filomenor@gmail.com>
2022-11-05 19:35:08 +0200

    Moved bottom bar to widgets

--- flutter_demo/lib/screens/profile_screen.dart
+++ flutter_demo/lib/screens/profile_screen.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import '../models/chat_model.dart';
 import '../models/message_model.dart';
 import '../models/user_model.dart';
+import '../widgets/bottom_bar.dart';
 import '../widgets/main_container.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -26,138 +27,32 @@ class ProfileScreen extends StatelessWidget {
             Theme.of(context).colorScheme.secondary,
           ])),
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'My Profile',
-            style: Theme.of(context)
-                .textTheme
-                .titleLarge!
-                .copyWith(fontWeight: FontWeight.bold),
-          ),
-          centerTitle: false,
-          elevation: 0,
-        ),
+        // appBar: AppBar(
+        //   title: Text(
+        //     'My Profile',
+        //     style: Theme.of(context)
+        //         .textTheme
+        //         .titleLarge!
+        //         .copyWith(fontWeight: FontWeight.bold),
+        //   ),
+        //   centerTitle: false,
+        //   elevation: 0,
+        // ),
         body: Column(children: [
+          const SizedBox(height: 30),
           _SocialBar(height: height, users: users),
           Expanded(
             child: Stack(
               fit: StackFit.expand,
               children: [
-                _ChatPreview(height: height, chats: chats),
-                _BottomBar(width: width),
+                // _ChatPreview(height: height, chats: chats),
+                BottomBar(width: width),
               ],
             ),
           ),
         ]),
       ),
     );
-  }
-}
-
-class _BottomBar extends StatelessWidget {
-  const _BottomBar({Key? key, required this.width}) : super(key: key);
-
-  final double width;
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        height: 65,
-        width: width * 0.5,
-        margin: const EdgeInsets.only(bottom: 30),
-        decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Theme.of(context).colorScheme.primary.withAlpha(150),
-                spreadRadius: 4,
-                blurRadius: 7,
-                offset: const Offset(0, 3),
-              )
-            ],
-            borderRadius: BorderRadius.circular(15),
-            color: Theme.of(context).colorScheme.primary.withAlpha(200)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Material(
-              color: Colors.transparent,
-              child: IconButton(
-                  onPressed: () {
-                    Get.toNamed('/profile');
-                  },
-                  icon: const Icon(
-                      color: Colors.black, Icons.person_outline_outlined)),
-            ),
-            const SizedBox(width: 30),
-            Material(
-              color: Colors.transparent,
-              child: IconButton(
-                  onPressed: () {
-                    Get.toNamed('/social');
-                  },
-                  icon:
-                      const Icon(color: Colors.black, Icons.message_outlined)),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _ChatPreview extends StatelessWidget {
-  const _ChatPreview({
-    Key? key,
-    required this.height,
-    required this.chats,
-  }) : super(key: key);
-
-  final double height;
-  final List<Chat> chats;
-
-  @override
-  Widget build(BuildContext context) {
-    return MainContainer(
-        height: height,
-        child: ListView.builder(
-          itemCount: chats.length,
-          itemBuilder: (context, index) {
-            User user =
-                chats[index].users!.where((user) => user.id != '1').first;
-            chats[index]
-                .messages
-                .sort((a, b) => b.createdAt.compareTo(a.createdAt));
-            Message lastMessage = chats[index].messages.first;
-
-            return ListTile(
-              onTap: () {
-                Get.toNamed('/chat', arguments: [
-                  user,
-                  chats[index],
-                ]);
-              },
-              leading: CircleAvatar(
-                radius: 30,
-                backgroundImage: AssetImage(user.imagePath),
-              ),
-              title: Text(
-                '${user.name} ${user.surname}',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge!
-                    .copyWith(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(lastMessage.text,
-                  maxLines: 1, style: Theme.of(context).textTheme.bodySmall),
-              trailing: Text(
-                  '${lastMessage.createdAt.hour}:${lastMessage.createdAt.minute}',
-                  style: Theme.of(context).textTheme.bodySmall),
-            );
-          },
-        ));
   }
 }
 
--- flutter_demo/lib/screens/social_screen.dart
+++ flutter_demo/lib/screens/social_screen.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import '../models/chat_model.dart';
 import '../models/message_model.dart';
 import '../models/user_model.dart';
+import '../widgets/bottom_bar.dart';
 import '../widgets/main_container.dart';
 
 class SocialScreen extends StatelessWidget {
@@ -44,65 +45,11 @@ class SocialScreen extends StatelessWidget {
               fit: StackFit.expand,
               children: [
                 _ChatPreview(height: height, chats: chats),
-                _BottomBar(width: width),
+                BottomBar(width: width),
               ],
             ),
           ),
         ]),
-      ),
-    );
-  }
-}
-
-class _BottomBar extends StatelessWidget {
-  const _BottomBar({Key? key, required this.width}) : super(key: key);
-
-  final double width;
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        height: 65,
-        width: width * 0.5,
-        margin: const EdgeInsets.only(bottom: 30),
-        decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Theme.of(context).colorScheme.primary.withAlpha(150),
-                spreadRadius: 4,
-                blurRadius: 7,
-                offset: const Offset(0, 3),
-              )
-            ],
-            borderRadius: BorderRadius.circular(15),
-            color: Theme.of(context).colorScheme.primary.withAlpha(200)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Material(
-              color: Colors.transparent,
-              child: IconButton(
-                  onPressed: () {
-                    Get.toNamed('/profile');
-                  },
-                  icon: const Icon(
-                      color: Colors.black, Icons.person_outline_outlined)),
-            ),
-            const SizedBox(width: 30),
-            Material(
-              color: Colors.transparent,
-              child: IconButton(
-                  onPressed: () {
-                    Get.toNamed('/social');
-                  },
-                  icon:
-                      const Icon(color: Colors.black, Icons.message_outlined)),
-            ),
-          ],
-        ),
       ),
     );
   }
