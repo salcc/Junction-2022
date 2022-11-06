35d0b93baee41dd094d016dc2a07b82491acf1f3
Marçal Comajoan Cara <mcomajoancara@gmail.com>
2022-11-06 02:10:27 +0100

    Avoid recomputation and improve variable names

--- matching_model/matching_model.py
+++ matching_model/matching_model.py
@@ -4,12 +4,12 @@ import torch.nn as nn
 from torch import Tensor
 from tqdm import tqdm
 
-from matching_data import get_evolution_exponential_weighted_decay, get_tokenization, profiles_feature_extraction
+from matching_data import get_evolution_exponential_weighted_decay, get_tokenization, profiles_feature_extraction, get_all_user_ids
 
 
-def embeddings_distance(embeddings1: Tensor, embeddings2: Tensor):
+def embeddings_distance(flattened_embeddings1: Tensor, flattened_embeddings2: Tensor):
   cos_sim = nn.CosineSimilarity(dim=1, eps=1e-6)
-  return cos_sim(embeddings1, embeddings2)
+  return cos_sim(flattened_embeddings1, flattened_embeddings2)
 
 
 class MatchingLoss(nn.Module):
@@ -21,19 +21,19 @@ class MatchingLoss(nn.Module):
     self.tau = Tensor([tau])
     self.sigmoid = nn.Sigmoid()
 
-  def forward(self, user_embeddings: Tensor, reciever_embeddings: Tensor, feedback: int, evolution: float, user_profile_features: Tensor, reciever_profile_features: Tensor) -> float:
+  def forward(self, user_embeddings: Tensor, peer_embeddings: Tensor, feedback: int, evolution: float, user_profile_features: Tensor, peer_profile_features: Tensor) -> float:
     feedback = Tensor([feedback])
     evolution = Tensor([evolution])
 
     flattened_user_embeddings = user_embeddings.view(1, -1)
-    flattened_reciever_embeddings = reciever_embeddings.view(1, -1)
+    flattened_peer_embeddings = peer_embeddings.view(1, -1)
 
     complete_user_embeddings = torch.cat((flattened_user_embeddings, user_profile_features), 1)
-    complete_reciever_embeddings = torch.cat((flattened_reciever_embeddings, reciever_profile_features), 1)
+    complete_peer_embeddings = torch.cat((flattened_peer_embeddings, peer_profile_features), 1)
 
     switch = torch.floor(self.tau + self.sigmoid(self.alpha * feedback + self.beta * evolution))
-    return switch * embeddings_distance(complete_user_embeddings, complete_reciever_embeddings)**2 \
-          + switch * max(0, self.margin - embeddings_distance(complete_user_embeddings, complete_reciever_embeddings))**2
+    distance = embeddings_distance(complete_user_embeddings, complete_peer_embeddings)
+    return switch * distance**2 + switch * max(0, self.margin - distance)**2
 
 
 def retrain(user_id: int, group_peers_ids: List[int], user_feedback: int, tokenizer, optimizer, model, matching_loss, device):
