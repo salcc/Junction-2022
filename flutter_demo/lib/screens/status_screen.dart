c4003fa9204260fa64758f6a05e2f6a7afbfa7ff
Marc Franquesa <marc.franquesam@gmail.com>
2022-11-06 02:29:03 +0100

    Update status screen format

--- flutter_demo/lib/screens/status_screen.dart
+++ flutter_demo/lib/screens/status_screen.dart
@@ -48,33 +48,27 @@ class _Salute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: height * 0.2,
-        margin: const EdgeInsets.only(left: 20, top: 150),
+        margin: const EdgeInsets.only(left: 2, top: 150),
         child: Row(
           children: [
             Container(
               alignment: Alignment.centerLeft,
               margin: const EdgeInsets.only(left: 20, top: 10),
-              width: width * 0.475,
+              width: width * 0.9,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     'Hi ${user.name} ${user.surname}!',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge!
-                        .copyWith(fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
                     textAlign: TextAlign.left,
                   ),
                   const SizedBox(height: 10),
                   Text('How are you doing today?',
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium!
-                          .copyWith(fontWeight: FontWeight.bold),
+                      style: Theme.of(context).textTheme.titleLarge!,
                       textAlign: TextAlign.left),
-                  const SizedBox(height: 10),
+                  const SizedBox(height: 5),
                 ],
               ),
             )
@@ -93,9 +87,8 @@ class _Emotions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height * 0.15,
-      margin: const EdgeInsets.only(left: 20, top: 10),
+    return StatusContainer(
+      height: 100,
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
           shrinkWrap: true,
@@ -107,16 +100,45 @@ class _Emotions extends StatelessWidget {
                   Get.toNamed('/profile');
                 },
                 icon: Container(
-                  margin: const EdgeInsets.only(right: 15),
+                  margin: const EdgeInsets.only(right: 2, left: 2),
                   width: 60,
                   height: 60,
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
                     shape: BoxShape.circle,
                     color: colors[index],
                   ),
                 ));
           }),
+    );
+  }
+}
+
+class StatusContainer extends StatelessWidget {
+  const StatusContainer({
+    Key? key,
+    required this.height,
+    required this.child,
+  }) : super(key: key);
+
+  final double height;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      margin: const EdgeInsets.only(left: 10, right: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
+          bottomRight: Radius.circular(25),
+        ),
+        color: Theme.of(context).colorScheme.background.withOpacity(0.8),
+      ),
+      child: child,
     );
   }
 }
